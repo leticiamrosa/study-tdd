f82204cf677fcb4f1d39e021a1aa0ab4cd3e8155
leticia monteiro <leticia@vizir.com.br>
2019-10-28 10:35:58 -0300

    create: test counter

--- src/components/counter/counter.test.js
+++ src/components/counter/counter.test.js
@@ -1,9 +1,46 @@
-import React from "react";
-import Counter from "./index";
-import { shallow } from "enzyme";
+import React from 'react';
+import Counter from './index';
+import { shallow } from 'enzyme';
 
-it("render without crashing", () => {
-  const wrapper = shallow(<Counter />);
-  const appComponent = wrapper.find("[data-test='component-counter'");
+/**
+ * factory function to create a ShallowWrapper for the App component
+ * @function
+ * @param {object} props - Component props specific to this setup
+ * @param {any} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  return shallow(<Counter {...props} />);
+};
+
+/**
+ * Return ShallowRapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {String} val - Value of data-test attribute for seach
+ * @returns {ShallowWrapper}
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+it('render without crashing', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-counter');
   expect(appComponent.length).toBe(1);
 });
+
+it('render increment button', () => {
+  const wrapper = shallow(<Counter />);
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+it('render counter display', () => {
+  const wrapper = shallow(<Counter />);
+  const display = findByTestAttr(wrapper, 'counter-display');
+  expect(display.length).toBe(1);
+});
+
+it('counter start at 0', () => {});
--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -9,7 +9,8 @@ export default class Counter extends Component {
   render() {
     return (
       <div data-test="component-counter">
-        <h1>App</h1>
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment Counter</button>
       </div>
     );
   }
