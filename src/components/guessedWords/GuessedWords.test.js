9678d4e007b4caa4634a12669490d87c047ebb46
leticiamrosa <leticia@vizir.com.br>
2020-02-07 10:13:37 -0300

    tests and code for non-zero guessed words

--- src/components/guessedWords/GuessedWords.test.js
+++ src/components/guessedWords/GuessedWords.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../../../test/testUtils';
 import GuessedWords from './index';
 
-const defaultProps = {
-  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+let defaultProps = {
+  guessedWords: [
+    { guessedWord: 'train', letterMatchCounter: 3},
+    { guessedWord: 'agile', letterMatchCounter: 1},
+    { guessedWord: 'party', letterMatchCounter: 5},
+  ]
 };
+
 
 /**
  * Factory function to create a ShallowWrapper for the GuessedWords component
@@ -25,7 +30,7 @@ it('doesnt throw warning with expected props', () => {
 
 describe('if there are no words guessed', () => {
   let wrapper;
-
+  
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] }); //both test can access wrapper
   });
@@ -41,4 +46,25 @@ describe('if there are no words guessed', () => {
   });
 });
 
-describe('if there are words guessed', () => {});
+describe('if there are words guessed', () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = setup(); //both test can access wrapper
+  });
+
+  it('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  it('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  it('correct number of guessed words', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNode.length).toBe(defaultProps.guessedWords.length)
+  })
+});
--- src/components/guessedWords/index.js
+++ src/components/guessedWords/index.js
@@ -8,6 +8,30 @@ function GuessedWords({ guessedWords }) {
     contents = (
       <span data-test="guess-intructions">Try to guess the secret word</span>
     );
+  } else {
+    const guessedWordsRows = guessedWords && guessedWords.map((word, i) => (
+      <tr data-test="guessed-word" key={i}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letter</th>
+            </tr>
+          </thead>
+          <tbody>
+            { guessedWordsRows }
+          </tbody>
+        </table>
+      </div>
+    )
   }
 
   return <div data-test="component-guessed-words">{contents}</div>;
